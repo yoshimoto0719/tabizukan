1a917311f12acb1d6ba10e1292dda5eb56309a3c
yoshimoto0719 <115342937+yoshimoto0719@users.noreply.github.com>
2022-12-21 04:49:45 +0000

    [gem_seedインストール]

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Admin.create!(
+  email: 'ysmt.0719@gmail.com',
+  password: 'Yuta0719'
+  )
