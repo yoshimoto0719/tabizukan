fbfe56f302ef2533a92e5568ae348bc3d7a0aece
yoshimoto0719 <115342937+yoshimoto0719@users.noreply.github.com>
2022-12-27 05:19:00 +0000

    [TOPページ作成（一覧ページ)]

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -15,7 +15,21 @@ class Public::PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to post_path(@post.id)
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to '/'
+  end
+
 
   private
 
