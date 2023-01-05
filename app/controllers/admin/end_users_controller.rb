17e5f1360065dad526c378e8b92d79ae8a0155ba
yoshimoto0719 <115342937+yoshimoto0719@users.noreply.github.com>
2023-01-05 04:58:48 +0000

    [サイドバーレイアウト修正]

--- app/controllers/admin/end_users_controller.rb
+++ app/controllers/admin/end_users_controller.rb
@@ -5,6 +5,26 @@ class Admin::EndUsersController < ApplicationController
   end
 
   def show
+    @end_user = EndUser.find(params[:id])
   end
+
+  def edit
+    @end_user = EndUser.find(params[:id])
+  end
+
+  def update
+    @end_user = EndUser.find(params[:id])
+    if @end_user.update(end_user_params)
+      redirect_to admin_end_user_path(@end_user)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+   def end_user_params
+    params.require(:end_user).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :is_deleted)
+   end
 
 end
