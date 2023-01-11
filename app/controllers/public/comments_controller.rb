5499e082b1004b59c57bfb0b865d3ac5cc3ae86b
yoshimoto0719 <115342937+yoshimoto0719@users.noreply.github.com>
2023-01-11 05:00:24 +0000

    [コメント機能　controller作成]

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -1,2 +1,22 @@
 class Public::CommentsController < ApplicationController
+  before_action :authenticate_end_user!
+
+  def create
+    post = Post.find(params[:post_id])
+    @comment = post.comments.build(comment_params)
+    @comment.end_user_id = current_end_user.id
+    if @comment.save
+      flash[:success] = "コメントしました"
+      redirect_back(fallback_location: root_path)
+    else
+      flash[:success] = "コメントできませんでした"
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
+  private
+
+    def comment_params
+      params.require(:comment).permit(:content)
+    end
 end
